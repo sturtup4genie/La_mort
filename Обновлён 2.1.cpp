29175ce5db8e3787001be0d6815f39668e9e631a
Родион Речкалов <91637521+sturtup4genie@users.noreply.github.com>
2021-12-08 19:17:14 +0300

    Update Обновлён 2.1.cpp

--- Обновлён 2.1.cpp
+++ Обновлён 2.1.cpp
@@ -9,33 +9,24 @@ using namespace std;
 *\param b второе число.
 *\return сумму.
 */
-double getAmount(const double a, const double b)
-{
-	return a + b;
-}
+double getAmount(const double a, const double b);
 /**
 *\brief функция вычисляет разность.
 *\param a первое число.
 *\param b второе число.
 *\return разность.
 */
-double getDifference(const double a, const double b)
-{
-	return a - b;
-}
+double getDifference(const double a, const double b);
 /**
 *\brief функция вычисляет среднее арифметическое.
 *\param a первое число.
 *\param b второе число.
 *\return среднее арифметическое.
 */
-double getArithmeticMean(const double a, const double b)
-{
-	return (a + b)/2;
-}
+double getArithmeticMean(const double a, const double b);
 /**
 *\brief Выбор расчёта суммы(0),разности(1),среднего арифметического(2).
-*\return 0 в случае успеха
+*\return 0 в случае успеха.
 */
 enum class MathFunct
 {
@@ -70,4 +61,19 @@ void main()
 		break;
 	default:
 	}
+}
+
+double getAmount(const double a, const double b)
+{
+	return a + b;
+}
+
+double getDifference(const double a, const double b)
+{
+	return a - b;
+}
+
+double getArithmeticMean(const double a, const double b)
+{
+	return (a + b)/2;
 }
