a63d48234efb076d066b80c1552977a7f149636e
Родион Речкалов <91637521+sturtup4genie@users.noreply.github.com>
2021-12-08 19:12:21 +0300

    Update Обновлён 1.2.cpp

--- Обновлён 1.2.cpp
+++ Обновлён 1.2.cpp
@@ -9,20 +9,14 @@ using namespace std;
 *\param cathet2 второй катет треугольника.
 *\return площадь треугольника.
 */
-double Square(const double cathet1, const double cathet2)
-{
-	return cathet1 * 0, 5 * cathet2;
-}
+double Square(const double cathet1, const double cathet2);
 /**
 *\brief функция вычисляет третий катет треугольника.
 *\param cathet1 первый катет треугольника.
 *\param cathet2 второй катет треугольника.
 *\return третий катет треугольника.
 */
-double Thirdcathet(const double cathet1, const double cathet2)
-{
-	return(sqrt(pow(cathet1, 2) + pow(cathet2, 2)));
-}
+double thirdcathet(const double cathet1, const double cathet2);
 /**
 *\brief функция вычисляет периметр треугольника.
 *\param cathet1 первый катет треугольника.
@@ -30,10 +24,7 @@ double Thirdcathet(const double cathet1, const double cathet2)
 *\param cathet3 третий катет треугольника.
 *\return периметр.
 */
-double Perimeter(const double cathet1, const double cathet2, const double cathet3)
-{
-	return cathet1 + cathet2 + cathet3;
-}
+double Perimeter(const double cathet1, const double cathet2, const double cathet3);
 
 /**
 *\brief точка входа в программу.
@@ -47,9 +38,24 @@ void main()
 	cin >> cathet1;
 	double cathet2;
 	cout << "Введите второй катет ";
-	cin >> cathet2;	
+	cin >> cathet2;
 	double cathet3;
-	cathet3=Thirdcathet(cathet1,cathet2);
+	cathet3=thirdcathet(  cathet1,  cathet2);
 	cout << "Площадь равна " << Square(cathet1, cathet2) << " см" << endl;
 	cout << "Периметр равен " << Perimeter(cathet1, cathet2, cathet3) << " см" << endl;
+}
+
+double Square(const double cathet1, const double cathet2)
+{
+	return cathet1 * 0, 5 * cathet2;
+}
+
+double thirdcathet(const double cathet1, const double cathet2)
+{
+	return(sqrt(pow(cathet1, 2) + pow(cathet2, 2)));
+}
+
+double Perimeter(const double cathet1, const double cathet2, const double cathet3)
+{
+	return cathet1 + cathet2 + cathet3;
 }
