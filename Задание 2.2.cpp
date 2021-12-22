4bfe0054f9bc19900737751e6c36803f80b7f823
Родион Речкалов <91637521+sturtup4genie@users.noreply.github.com>
2021-12-22 23:26:11 +0300

    Написан код для задачи 2.2

---
+++ Задание 2.2.cpp
@@ -0,0 +1,61 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+const double a = 2;
+
+/**
+*\brief математическая функция, работающая для ограничений x > a.
+*\param const double x - значение параметра x.
+*\return возвращает значение функции.
+**/
+
+double getFunctionFirst(const double x);
+
+/**
+*\brief математическая функция, работающая для ограничений x <= a.
+*\param const double x - значение параметра x.
+*\return возвращает значение функции.
+**/
+
+double getFunctionSecond(const double x);
+
+/**
+*\brief точка входа в программу.
+*\return  0 в случае успеха.
+*/
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double x;
+	std::cout << "Введите значение = ";
+	std::cin >> x;
+	double y;
+
+	if (x <= a)
+	{
+		y = getFunctionSecond(x);
+	}
+	else if (x > a)
+	{
+		y = 1;
+	}
+	else
+	{
+		y = getFunctionFirst(x);
+	}
+	std::cout << "Y = " << y;
+	return 0;
+}
+
+double getFunctionFirst(const double x)
+{
+	return tan(x) + M_PI / 3;
+}
+
+
+double getFunctionSecond(const double x)
+{
+	return sin(x)*a;
+}
