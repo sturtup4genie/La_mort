bcc3441c49706d5867089eae8ff7f3b4d448b9e4
Родион Речкалов <91637521+sturtup4genie@users.noreply.github.com>
2022-01-20 21:11:30 +0300

    Update 4.1.cpp

--- 4.1.cpp
+++ 4.1.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <ctime>
-
+/**
+* \brief перечислимый тип способов задания массива
+*/
+enum class type_of_filling
+{
+	/**
+	* \brief генератор случайных чисел
+	*/
+	rand = 1,
+	/**
+	* \brief ввод вручную
+	*/
+	user = 2
+};
 /**
 * \brief заполнение массива случайными числами
 * \param arr массив
 * \param size размер массива
 **/
-void automatic_filling(int size, int arr[]);
+void automatic_filling(int* arr, const size_t size);
 /**
 * \brief заполнение массива вручную
 * \param arr массив
 * \param size размер массива
 **/
-void filling_in_manually(int size, int arr[]);
+void filling_in_manually(int* arr, const size_t size);
 
 /**
 * \brief функция для нахождения суммы элементов массива >0
@@ -34,9 +47,21 @@ void get_elements(int* arr, const size_t size);
 * \param size размер массива
 * \param input поиск пары меньше этого числа
 **/
-void get_pair(int* arr, const size_t size, double input);
+void get_pair(int* arr, const size_t size, int input);
+/**
+* \brief ввод массива с клавиатуры
+* \param arr массив
+* \param size размер массива
+**/
+void user_input(int* arr, const size_t size);
+/**
+* \brief вывод массива
+* \param arr Массив
+* \param size Размер массива
+**/
+void print(int* arr, const size_t size);
 
-int main() 
+int main()
 {
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
@@ -44,16 +69,22 @@ int main()
 	std::cout << "Введите размер массива" << std::endl;
 	std::cin >> size;
 	int* arr = new int[size];
-	int choise;
-	std::cout << "\n0-автоматическое заполнение массива\n1-заполнить массив вручную\n" << std::endl;
-	std::cin >> choise;
-	if (choise == 0)
+	std::cout << "1 - Сгенерировать массив\n2 - Заполнить массив вручную\n";
+	int choice;
+	std::cin >> choice;
+	const auto filling = static_cast<type_of_filling>(choice);
+	switch (filling)
 	{
-		automatic_filling(size, arr);
-	}
-	else
-	{
-		filling_in_manually(size, arr);
+	case type_of_filling::rand:
+		automatic_filling(arr, size);
+		print(arr, size);
+		break;
+	case type_of_filling::user:
+		user_input(arr, size);
+		print(arr, size);
+		break;
+	default:
+		std::cout << "Ошибка! Не выбран ни один из вариантов";
 	}
 	std::cout << "Сумма положительных элементов: " << get_sum(arr, size) << "\n\n";
 
@@ -62,7 +93,7 @@ int main()
 	get_elements(arr, size);
 
 	std::cout << "Найти номер первой пары соседних элементов с разными знаками, сумма которых меньше заданного числа: \n";
-	double input;
+	int input;
 	std::cin >> input;
 	get_pair(arr, size, input);
 
@@ -75,22 +106,22 @@ int main()
 	return 0;
 }
 
-void automatic_filling(int size, int arr[])
+void automatic_filling(int* arr, const size_t size)
 {
-	for (int i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		arr[i] = rand() % 20;
 	}
-	for (int i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		std::cout << arr[i] << std::endl;
 	}
 }
 
-void filling_in_manually(int size, int arr[])
+void filling_in_manually(int* arr, const size_t size)
 {
 	std::cout << "Введите значения" << std::endl;
-	for (int i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		std::cin >> arr[i];
 	}
@@ -102,12 +133,12 @@ void filling_in_manually(int size, int arr[])
 }
 
 
-int get_sum(int* arr, const size_t size) 
+int get_sum(int* arr, const size_t size)
 {
 	int sum = 0;
-	for (size_t i = 0; i < size; i++) 
+	for (size_t i = 0; i < size; i++)
 	{
-		if ((arr[i] >= 0) && (arr[i] > 9) && (arr[i] < 100)) 
+		if ((arr[i] >= 0) && (arr[i] > 9) && (arr[i] < 100))
 		{
 			sum += arr[i];
 		}
@@ -117,7 +148,7 @@ int get_sum(int* arr, const size_t size)
 
 void get_elements(int* arr, const size_t size)
 {
-	for (size_t i = 0; i < size; i++) 
+	for (size_t i = 0; i < size; i++)
 	{
 		if ((arr[i] % 2 == 0) && (arr[i] >= 0))
 		{
@@ -130,19 +161,36 @@ void get_elements(int* arr, const size_t size)
 }
 
 
-void get_pair(int* arr, const size_t size, double input)
+void get_pair(int* arr, const size_t size, int input)
 {
 	bool k = false;
-	for (size_t i = 1; i < size; i++) 
+	for (size_t i = 1; i < size; i++)
 	{
-		if ((arr[i] * arr[i - 1] < 0) && (arr[i] + arr[i - 1]<input)) 
+		if ((arr[i] * arr[i - 1] < 0) && (arr[i] + arr[i - 1]<input))
 		{
 			std::cout << "Это пара под номером: " << i << " " << i - 1 << " ";
 			k = true;
 		}
 	}
-	if (k == false) 
+	if (k == false)
 	{
 		std::cout << "Таких пар нет ";
 	}
+}
+void user_input(int* arr, const size_t size)
+{
+	for (size_t i = 0; i < size; i++) 
+	{
+		std::cout << "arr[" << i << "] = ";
+		std::cin >> arr[i];
+	}
+}
+void print(int* arr, const size_t size) 
+{
+	std::cout << "array[" << size << "]" << "= {";
+	for (size_t i = 0; i < size - 1; i++) 
+	{
+		std::cout << arr[i] << ";";
+	}
+	std::cout << arr[size - 1] << "}";
 }
